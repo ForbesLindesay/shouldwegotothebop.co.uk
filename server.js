7d693ba8cc75146a5b6b3ee6f058a29d36c12403
ForbesLindesay <forbes@lindesay.co.uk>
2013-06-18 16:11:21 +0100

    Release 2.0.0 (toned down a lot)

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var path = require('path')
 var fs = require('fs')
 
-var browserify = require('browserify-middleware')
 var express = require('express')
 var app = express()
 
@@ -16,19 +15,21 @@ app.get('/', function (req, res) {
   html = html.replace(/{{version}}/g, version)
   res.send(html)
 })
+
 app.get('/bop.manifest', function (req, res) {
   res.type('text/manifest')
   var html = fs.readFileSync(__dirname + '/bop.manifest', 'utf8')
   html = html.replace(/{{version}}/g, version)
+             .replace(/{{time}}/g, (new Date).toISOString())
   res.send(html)
 })
+
 app.get('/static/' + version + '/style.css', function (req, res) {
   res.type('css')
   var css = fs.readFileSync(__dirname + '/style.css', 'utf8')
   css = css.replace(/{{version}}/g, version)
   res.send(css)
 })
-app.get('/static/' + version + '/client.js', browserify('./client.js'))
 app.get('/static/' + version + '/background.png', file('./background.png'))
 app.get('/static/' + version + '/image.png', file('./image.png'))
 
